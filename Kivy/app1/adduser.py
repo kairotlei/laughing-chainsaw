aa411479c5ef915f135af7b83091e6b5755e2a0d
kairotlei <ygit config --global user.name Your>
2022-01-18 10:04:07 +0100

    aaa

--- Kivy/app1/adduser.py
+++ Kivy/app1/adduser.py
@@ -3,30 +3,46 @@ from kivy.lang import Builder
 
 class MainAPP(MDApp):
     def build(self):
+        # Tema oscuro
         #self.theme_cls.theme_style = "Dark"
+
+        # Color principal = naranja
         self.theme_cls.primary_palette = "Orange"
+
+        # Cargar .KV
         return Builder.load_file("adduser.kv")    
     
+
+    # Guardado de datos
     def save_data(self, user_name, user_surname, user_course, user_email, user_telegram):
-        # Guardado de datos
+        
+        # Guardar inputs de Kivy en variables
         name = self.root.ids.user_name.text
         surname = self.root.ids.user_surname.text
         course = self.root.ids.user_course.text
         email = self.root.ids.user_email.text
         telegram = self.root.ids.user_telegram.text
         
+        # alumno = [datos que conforman el alumno]
         aln = [name, surname, course, email, telegram]
         
-        with open("bd_alumnos.txt", "a+") as file:
-            print("abriendo archivo...")
-            for dato in aln:
-               file.write("%s; " %dato)
-               print("escribiendo:  %s\n" %dato)
-            file.write("\n") 
+        # Intentar, si falla algo se notifica.
+        try:
+            # Pasar datos a archivo (texto plano, no DB!!)
+            # Abrir archivo o crearlo si no existe.
+            with open("bd_alumnos.csv", "a+") as file:
+                # Pasar cada dato de alumno al archivo, con ";" entre variables para que resulte en un .csv
+                # Cada [salto de] línea es un alumno distinto, así que meter un "\n" tras procesar la línea
+                for dato in aln:
+                    file.write("%s; " %dato)
+                    print("escribiendo:  %s\n" %dato)
+                    file.write("\n")
+            file.close()
+            print("fin")
 
-                      
-        file.close()
-        print("fin")
+        # En caso de fallo...
+        except:
+            print(" ")
 
         self.root.ids.user_name.text = "" 
         self.root.ids.user_surname.text = "" 
