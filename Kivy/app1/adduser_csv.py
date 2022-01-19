42565086b3a5842c58f078c9de06bc8da97aa5c2
kairotlei <ygit config --global user.name Your>
2022-01-19 21:20:48 +0100

    aaa

---
+++ Kivy/app1/adduser_csv.py
@@ -0,0 +1,59 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+import csv
+
+class MainAPP(MDApp):
+    def build(self):
+        #self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
+        return Builder.load_file("adduser_csv.kv")    
+    
+    def save_data(self, user_name, user_surname, user_course, user_email, user_telegram):
+        # Guardado de datos
+        name = self.root.ids.user_name.text
+        surname = self.root.ids.user_surname.text
+        course = self.root.ids.user_course.text
+        email = self.root.ids.user_email.text
+        telegram = self.root.ids.user_telegram.text
+        
+        aln = [(name, surname, course, email, telegram)]
+        
+        with open("bd_alumnos.csv", "a+",newline="\n") as file_csv:
+            keys = ["Name", "Surname", "Course", "Email", "Telegram"]
+            writer = csv.DictWriter(file_csv,fieldnames=keys)
+            #writer.writeheader()
+            for name, surname, course, email, telegram in aln:
+                writer.writerow({"Name": name, "Surname":surname, "Course":course, "Email":email, "Telegram":telegram})
+        print("Se guardaron los datos del alumno")
+
+                      
+        file_csv.close()
+        print("fin")
+
+        self.root.ids.user_name.text = "" 
+        self.root.ids.user_surname.text = "" 
+        self.root.ids.user_course.text = "" 
+        self.root.ids.user_email.text = ""         
+        self.root.ids.user_telegram.text = "" 
+    
+    def exit(self):
+        exit()
+    
+    
+if __name__ == '__main__':
+    MainAPP().run()
+
+    
+    
+    
+#  ------------------------------------
+#            
+#      inp. NOMBRE
+#      inp. APELLIDOS
+#      inp. CURSO
+#      inp. EMAIL
+#
+#
+#    bt.LOGIN                 bt.CLEAR
+# --------------------------------------
+#                                      MDCard
